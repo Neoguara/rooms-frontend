784d86730ada2c3af5c4d5a32bc0adcadfe45669
JuniorJose-git <joseronaldosantus@gmail.com>
2026-05-17 23:49:07 -0300

    .

--- src/routes/_authenticated/route.tsx
+++ src/routes/_authenticated/route.tsx
@@ -1,8 +1,19 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router'
+import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
 import { AppSidebar } from '#/components/app-sidebar'
 import { SidebarInset, SidebarProvider } from '#/components/ui/sidebar'
 
 export const Route = createFileRoute('/_authenticated')({
+  beforeLoad: ({
+    context: {
+      authentication: { isLogged },
+    },
+  }) => {
+    if (!isLogged()) {
+      throw redirect({
+        to: '/login',
+      })
+    }
+  },
   component: RouteComponent,
 })
 
